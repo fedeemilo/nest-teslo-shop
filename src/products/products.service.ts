df00c4c57f9835a3c0301a80958913127fff2ebd
Federico Milone <federico.milone@claro.com.ar>
2024-03-22 19:02:10 -0300

    Complete create product service

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common'
+import {
+    BadRequestException,
+    Injectable,
+    InternalServerErrorException,
+    Logger
+} from '@nestjs/common'
 import { CreateProductDto } from './dto/create-product.dto'
 import { UpdateProductDto } from './dto/update-product.dto'
 import { Product } from './entities/product.entity'
@@ -7,25 +12,22 @@ import { Repository } from 'typeorm'
 
 @Injectable()
 export class ProductsService {
+    private readonly logger = new Logger('ProductsService')
+
     constructor(
         @InjectRepository(Product)
         private readonly productRepository: Repository<Product>
     ) {}
 
     async create(createProductDto: CreateProductDto) {
-
         try {
-            
             const product = this.productRepository.create(createProductDto)
             await this.productRepository.save(product)
 
             return product
-            
         } catch (error) {
-            console.log(error)
-            throw new InternalServerErrorException('Ayuda!')
+            this.handleDBExceptions(error)
         }
-
     }
 
     findAll() {
@@ -42,5 +44,15 @@ export class ProductsService {
 
     remove(id: number) {
         return `This action removes a #${id} product`
+    }
+
+    private handleDBExceptions(error: any) {
+        this.logger.error(error)
+
+        if (error.code === '23505') throw new BadRequestException(error.detail)
+
+        throw new InternalServerErrorException(
+            'Unexpected error, check server logs for more details'
+        )
     }
 }
